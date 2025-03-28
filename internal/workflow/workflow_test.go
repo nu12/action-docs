0f5ed051b4f55d305fe225c3d0901a2b92e09e49
nu12 <34694287+nu12@users.noreply.github.com>
2025-03-28 15:32:58 -0400

    Make tests DRY

--- internal/workflow/workflow_test.go
+++ internal/workflow/workflow_test.go
@@ -13,93 +13,103 @@ import (
 
 const errorf = "Error: %v. \nExpected: %v \nGot: %v"
 
-func TestParse(t *testing.T) {
-	tests := []struct {
-		name                       string
-		data                       string
-		expectedFilename           string
-		expectedIsReusableWorkflow bool
-		expectedName               string
-		expectedDescription        string
-	}{
-		{
-			name: "Workflow call",
-			data: `
+var testData = []struct {
+	name                       string
+	data                       string
+	filename                   string
+	expectedHash               string
+	expectedFilename           string
+	expectedIsReusableWorkflow bool
+	expectedName               string
+	expectedDescription        string
+}{
+	{
+		name: "Workflow call",
+		data: `
 name: 'Workflow name 1'
 description: 'Workflow description 1'
 on: 
-  workflow_call:
-    inputs: 
-      in1: 
-        description: 'Input1'
-        required: true
-      in2: 
-        description: 'Input2'
-        required: false
-    outputs:
-      out1:
-        description: 'Output1'
-        value: 'Hello'
-    secrets:
-      sec1:
-        required: true
+workflow_call:
+inputs: 
+  in1: 
+	description: 'Input1'
+	required: true
+  in2: 
+	description: 'Input2'
+	required: false
+outputs:
+  out1:
+	description: 'Output1'
+	value: 'Hello'
+secrets:
+  sec1:
+	required: true
 `,
-			expectedFilename:           "call.yml",
-			expectedIsReusableWorkflow: true,
-			expectedName:               "Workflow name 1",
-			expectedDescription:        "Workflow description 1",
-		},
-		{
-			name: "Workflow dispatch",
-			data: `
+		filename:                   ".github/workflows/dispatch.yml",
+		expectedHash:               "",
+		expectedFilename:           "call.yml",
+		expectedIsReusableWorkflow: true,
+		expectedName:               "Workflow name 1",
+		expectedDescription:        "Workflow description 1",
+	},
+	{
+		name: "Workflow dispatch",
+		data: `
 name: 'Workflow name 2'
 description: 'Workflow description 2'
 on: 
-  workflow_dispatch:
-    inputs: 
-      in1: 
-        description: 'Input1'
-        type: choice
-        default: 'one'
-        options:
-        - one
-        - two
+workflow_dispatch:
+inputs: 
+  in1: 
+	description: 'Input1'
+	type: choice
+	default: 'one'
+	options:
+	- one
+	- two
 `,
-			expectedFilename:           "dispatch.yml",
-			expectedIsReusableWorkflow: false,
-			expectedName:               "Workflow name 2",
-			expectedDescription:        "Workflow description 2",
-		},
-		{
-			name: "Workflow call with nil",
-			data: `
+		filename:                   ".github/workflows/dispatch.yml",
+		expectedHash:               "",
+		expectedFilename:           "dispatch.yml",
+		expectedIsReusableWorkflow: false,
+		expectedName:               "Workflow name 2",
+		expectedDescription:        "Workflow description 2",
+	},
+	{
+		name: "Workflow call with nil",
+		data: `
 name: 'Workflow name 3'
 description: 'Workflow description 3'
 on: 
-  workflow_call: {}
+workflow_call: {}
 `,
-			expectedFilename:           "call.yml",
-			expectedIsReusableWorkflow: true,
-			expectedName:               "Workflow name 3",
-			expectedDescription:        "Workflow description 3",
-		},
-		{
-			name: "Workflow dispatch with nil",
-			data: `
+		filename:                   ".github/workflows/dispatch.yml",
+		expectedHash:               "",
+		expectedFilename:           "call.yml",
+		expectedIsReusableWorkflow: true,
+		expectedName:               "Workflow name 3",
+		expectedDescription:        "Workflow description 3",
+	},
+	{
+		name: "Workflow dispatch with nil",
+		data: `
 name: 'Workflow name 4'
 description: 'Workflow description 4'
 on: 
-  workflow_dispatch: {}
+workflow_dispatch: {}
 `,
-			expectedFilename:           "dispatch.yml",
-			expectedIsReusableWorkflow: false,
-			expectedName:               "Workflow name 4",
-			expectedDescription:        "Workflow description 4",
-		},
-	}
+		filename:                   ".github/workflows/dispatch.yml",
+		expectedHash:               "",
+		expectedFilename:           "dispatch.yml",
+		expectedIsReusableWorkflow: false,
+		expectedName:               "Workflow name 4",
+		expectedDescription:        "Workflow description 4",
+	},
+}
 
+func TestParse(t *testing.T) {
 	log := logging.NewLogger()
-	for _, tt := range tests {
+	for _, tt := range testData {
 		t.Run(tt.name, func(t *testing.T) {
 			dir := t.TempDir()
 			absoluteFilename := dir + "/" + tt.expectedFilename
@@ -152,59 +162,8 @@ on:
 }
 
 func TestMarkdown(t *testing.T) {
-	tests := []struct {
-		name     string
-		data     string
-		filename string
-		expected string
-	}{
-		{
-			name: "Workflow call",
-			data: `
-name: 'Workflow name'
-description: 'Workflow description'
-on: 
-  workflow_call:
-    inputs: 
-      in1: 
-        description: 'Input1'
-        required: true
-      in2: 
-        description: 'Input2'
-        required: false
-    outputs:
-      out1:
-        description: 'Output1'
-        value: 'Hello'
-    secrets:
-      sec1:
-        required: true
-`,
-			filename: ".github/workflows/call.yml",
-			expected: "360289fb1c3e8e14b64cf0d592ebf21a",
-		},
-		{
-			name: "Workflow dispatch",
-			data: `
-name: 'Workflow name'
-description: 'Workflow description'
-on: 
-  workflow_dispatch:
-    inputs: 
-      in1: 
-        description: 'Input1'
-        type: choice
-        default: 'one'
-        options:
-        - one
-        - two
-`,
-			filename: ".github/workflows/dispatch.yml",
-			expected: "ec84abaf87f6ec0ab0f0c3208e493229",
-		},
-	}
 
-	for _, tt := range tests {
+	for _, tt := range testData {
 		t.Run(tt.name, func(t *testing.T) {
 			w := Workflow{}
 			err := yaml.Unmarshal([]byte(tt.data), &w)
@@ -213,9 +172,9 @@ on:
 			}
 			w.Filename = tt.filename
 
-			if tt.expected != helper.Hash(w.Markdown()) {
-				t.Errorf(errorf, "Markdown doesn't match", tt.expected, helper.Hash(w.Markdown()))
-				t.Errorf(w.Markdown())
+			if tt.expectedHash != helper.Hash(w.Markdown()) {
+				t.Errorf(errorf, "Markdown doesn't match", tt.expectedHash, helper.Hash(w.Markdown()))
+				t.Error(w.Markdown())
 			}
 		})
 	}
@@ -451,10 +410,6 @@ func TestGetInputs(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			inputs := tt.given.getInputs()
-
-			if inputs == nil {
-				t.Errorf("Inputs is nil")
-			}
 
 			if len(*inputs) != len(*tt.expectedInputs) {
 				t.Errorf(errorf, "Inputs length doesn't match", len(*tt.expectedInputs), len(*inputs))
